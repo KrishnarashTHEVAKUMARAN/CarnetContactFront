bc52b320ae2820fded9af4dcc523379c0e1b2080
Krishnarash <thevakumarankrishnarash@gmail.com>
2023-12-16 22:00:33 +0100

    2eme navbar

---
+++ src/app/navbarform/navbarform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarform',
+  templateUrl: './navbarform.component.html',
+  styleUrls: ['./navbarform.component.css']
+})
+export class NavbarformComponent {
+
+}
